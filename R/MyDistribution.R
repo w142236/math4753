b000c351b5446162ad77ad0cdc3b55bb063d2912
w142236 <william.kett-1@ou.edu>
2020-02-21 21:00:01 -0600

    Added a few more comments into the documentation and R file.

--- R/MyDistribution.R
+++ R/MyDistribution.R
@@ -1,8 +1,8 @@
 
 #' myncurve
 #'
-#' @param mu The mean of the distribution curve. Essentially defines the center of the curve of the distribution.
-#' @param sigma The standard deviation of the distribution curve. Used to
+#' @param mu The mean of the normal probability distribution curve. Essentially defines the center of the curve of the distribution of the probability.
+#' @param sigma The standard deviation of the normal probability distribution curve.
 #'
 #' @return a curve of the normal distribution (defined mean and standard deviation)
 #' @export
