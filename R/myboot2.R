d3a07cf226a0c54d59a71aabcab2b4df8035e486
w142236 <william.kett-1@ou.edu>
2020-03-29 03:03:02 -0500

    This returns a histogram with confidence intervals calculated given alpha and is plotted onto the histogram and returned. It also returns a list of some of the objects used to make the confidence intervals.

---
+++ R/myboot2.R
@@ -0,0 +1,43 @@
+#' Title
+#'
+#' @param iter The Number of times that the re-sampled sample is, well, re-sampled again and again and again
+#' @param x An array holding the values in the sample
+#' @param fun The function which is being used to create a statistic. For example, sd(), var(), mean(), etc...
+#' @param alpha The percent to subtract off of 100% when defining the confidence level of the confidence intervals being created
+#' @param cx  A parameter that that adjusts the text such that it modifies cex
+#' @param ... This just means that we can add parameters to whereever this shows up (the histogram), i.e. we can add things like histogram labels
+#'
+#' @return A lot! it always returns a list of parameters and a histogram with a confidence interval and point estimate plotted onto it.
+#' @export
+#'
+#' @examples
+#' myboot(x = rchisq(20,3), fun = "mean")
+myboot2<-function(iter=10000,x,fun="mean",alpha=0.05,cx=1.5,...){  #Notice where the ... is repeated in the code
+  n=length(x)   #sample size
+
+  y=sample(x,n*iter,replace=TRUE)
+  rs.mat=matrix(y,nr=n,nc=iter,byrow=TRUE)
+  xstat=apply(rs.mat,2,fun) # xstat is a vector and will have iter values in it
+  ci=quantile(xstat,c(alpha/2,1-alpha/2))# Nice way to form a confidence interval
+  # A histogram follows
+  # The object para will contain the parameters used to make the histogram
+  para=hist(xstat,freq=FALSE,las=1,
+            main=paste("Histogram of Bootstrap sample statistics","\n","alpha=",alpha," iter=",iter,sep=""),
+            ...)
+
+  #mat will be a matrix that contains the data, this is done so that I can use apply()
+  mat=matrix(x,nr=length(x),nc=1,byrow=TRUE)
+
+  #pte is the point estimate
+  #This uses whatever fun is
+  pte=apply(mat,2,fun)
+  abline(v=pte,lwd=3,col="Black")# Vertical line
+  segments(ci[1],0,ci[2],0,lwd=4)      #Make the segment for the ci
+  text(ci[1],0,paste("(",round(ci[1],2),sep=""),col="Red",cex=cx)
+  text(ci[2],0,paste(round(ci[2],2),")",sep=""),col="Red",cex=cx)
+
+  # plot the point estimate 1/2 way up the density
+  text(pte,max(para$density)/2,round(pte,2),cex=cx)
+
+  invisible(list(xstat = xstat, ci=ci,fun=fun,x=x))# Some output to use if necessary
+}
